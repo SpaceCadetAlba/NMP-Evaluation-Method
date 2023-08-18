99ee9b792cf3ebd8d0249cf56a6ce1d9bbb42f1f
SpaceCadetAlba <pc1095@york.ac.uk>
2023-08-18 23:12:56 +0100

    Update launch scripts

--- Batch Scripts/launch_A.bat
+++ Batch Scripts/launch_A.bat
@@ -1,32 +1,34 @@
 @ECHO OFF
-:: This batch is for RT Test Protocol endpoint A
+:: This batch is for iNMP Test Protocol endpoint A
 
 ::JACK
 ECHO Now starting JACK
-START /min "" "C:\Users\AudioLab\Desktop\Jack PortAudio.lnk"
-ECHO Please wait for Jack PortAudio to load...
+start /min "" "C:\Program Files (x86)\Jack\jackd.exe" -R -S -d portaudio -d "*AUDIO DEVICE NAME" -r 48000 -p 128
+ECHO Please wait for JACK to load...
 TIMEOUT 20
-START /min "" "C:\Users\AudioLab\Desktop\Jack Control.lnk"
+
+ECHO Now starting JACK Control
+START /min "" "C:\Program Files (x86)\Jack\qjackctl.exe"
 ECHO Please wait for Jack Control to load...
 TIMEOUT 20
 
 ::REAPER
 ECHO Now starting Reaper
-START "C:\Program Files\REAPER (x64)\reaper.exe" "C:\Users\AudioLab\Desktop\PatrickCairns_DecayEnvStudy_Distributable\Reaper Session\audioRendering.rpp"
+START "C:\Program Files\REAPER (x64)\reaper.exe" "C:\NMP-Evaluation-Method-main\Reaper Session\audioRendering.rpp"
 TIMEOUT 60
 
 ::MATLAB APP
 ECHO Now starting participant GUI
-START "C:\Program Files\MATLAB\R2021b\bin\matlab.exe" "C:\Users\AudioLab\Desktop\PatrickCairns_DecayEnvStudy_Distributable\Matlab Application\revisedRTAppv2.mlapp"
+START "C:\Program Files\MATLAB\R2021b\bin\matlab.exe" "C:\NMP-Evaluation-Method-main\Matlab Application\revisedRTAppv2.mlapp"
 TIMEOUT 60
 
 ::JACKTRIP
 ECHO Now starting Jacktrip participant connection
-START "" "C:\Users\AudioLab\Desktop\PatrickCairns_DecayEnvStudy_Distributable\Batch Scripts\jacktrip_endpointA.bat" /k
+START "" "C:\NMP-Evaluation-Method-main\Batch Scripts\jacktrip_endpointA.bat" /k
 TIMEOUT 60
 
+ECHO To also include the control room endpoint with talkback, press enter to continue
 PAUSE
-
-ECHO Now starting Jacktrip talkback connection
 ::JACKTRIP - TALKBACK
-START "" "C:\Users\AudioLab\Desktop\PatrickCairns_DecayEnvStudy_Distributable\Batch Scripts\jacktrip_TBA.bat" /k
+START "" "C:\NMP-Evaluation-Method-main\Batch Scripts\jacktrip_TBA.bat" /k
+PAUSE
--- Batch Scripts/launch_TB.bat
+++ Batch Scripts/launch_TB.bat
@@ -12,16 +12,6 @@ START /min "" "C:\Program Files (x86)\Jack\qjackctl.exe"
 ECHO Please wait for Jack Control to load...
 TIMEOUT 20
 
-::REAPER
-ECHO Now starting Reaper
-START "C:\Program Files\REAPER (x64)\reaper.exe" "C:\NMP-Evaluation-Method-main\Reaper Session\audioRendering.rpp"
-TIMEOUT 60
-
-::MATLAB APP
-ECHO Now starting participant GUI
-START "C:\Program Files\MATLAB\R2021b\bin\matlab.exe" "C:\NMP-Evaluation-Method-main\Matlab Application\revisedRTAppv2.mlapp"
-TIMEOUT 60
-
 ::JACKTRIP
 ECHO Now starting Jacktrip participant connection
 START "" "NMP-Evaluation-Method-main\Batch Scripts\jacktrip_endpointB.bat" /k
@@ -30,5 +20,6 @@ TIMEOUT 60
 ECHO To also include the control room endpoint with talkback, press enter to continue
 PAUSE
 ::JACKTRIP - TALKBACK
-START "" "NMP-Evaluation-Method-main\Batch Scripts\jacktrip_TBB.bat" /k
+START "" "NMP-Evaluation-Method-main\Batch Scripts\jacktrip_A_TB.bat" /k
+START "" "NMP-Evaluation-Method-main\Batch Scripts\jacktrip_B_TB.bat" /k
 PAUSE
